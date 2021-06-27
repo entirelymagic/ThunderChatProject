6a9a3b8259c3a9e92d435beb96bad15ddc7acb0c
elvis.munteanu <elvis.munteanu@gmail.com>
2021-06-27 15:48:14 +0300

    Added redis channel_layers to settings.

--- src/ThunderChantPlayground/settings.py
+++ src/ThunderChantPlayground/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from .private_keys_data import django_secret_private_key
+from .private_keys_data import django_secret_private_key, DB_NAME, DB_USER, DB_PASSWORD
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'ThunderChantPlayground.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -123,3 +128,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Added CHANNEL_LAYERS in order to run REDIS with Windows
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6399)],
+        },
+    },
+}
